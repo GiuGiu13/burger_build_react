0c666369fcde7198ea51a234ea7286253370cc8c
Giusy <luminal@ymail.com>
2019-09-17 15:13:44 +0100

    on click show modal

--- my-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,7 +27,8 @@ class BurgerBuilder extends Component {
             meat:0
         },
         totalPrice: 5,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState(ingredients){
@@ -73,6 +74,10 @@ class BurgerBuilder extends Component {
         this.setState({totalPrice: newPrice, ingredients:updatedIngredients})
         this.updatePurchaseState(updatedIngredients);
     }
+
+    purchaseHandler = () => {
+        this.setState({purchasing:true})
+    }
     
     render(){
         const disabledInfo = {
@@ -85,7 +90,7 @@ class BurgerBuilder extends Component {
 
         return(
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -95,6 +100,7 @@ class BurgerBuilder extends Component {
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
+                    ordered={this.purchaseHandler}
                 />
             </Aux>
 
