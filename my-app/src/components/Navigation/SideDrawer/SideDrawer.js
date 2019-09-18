c107ed131c08cfec007f906d1f2d7eb7e57d6b82
Giusy <luminal@ymail.com>
2019-09-18 10:32:31 +0100

    close menu on click

--- my-app/src/components/Navigation/SideDrawer/SideDrawer.js
+++ my-app/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.module.css'
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux'
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return(
-        <div className={classes.SideDrawer}>
-            {/* approch to change the height of the logo dynamically */}
-            {/* <Logo height="11%"/> */}
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                {/* approch to change the height of the logo dynamically */}
+                {/* <Logo height="11%"/> */}
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+            
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-           
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
